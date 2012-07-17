47925683aae905a40954bdf1c18d357e961600af
SKYNET <reskynetonline@gmail.com>
2012-07-17 22:00:01 +0400

    Первый коммит

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'dfd9aa9ab3c27b175b4ede76bb4e9ae1a6f185e92a15b58fbc29ed783959f04addcdbcc16387efef5e7a04e8344e7549f8432ea3eebf97374be0519e86603784'
